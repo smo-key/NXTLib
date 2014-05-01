653d58925d950bffd04e642c0cf534f9c3466f23
smo-key <pachachura.arthur@gmail.com>
2014-04-30 19:34:08 -0500

    Added Bluetooth Protocol

---
+++ nxtlib/src/Connect/Comm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NXTLib
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public abstract class Comm
+    {
+        /// <summary>
+        /// <para>Protocol constructor used for communication.</para>
+        /// </summary>
+        public Comm()
+            : base()
+        { }
+
+        /// <summary> 
+        /// Connect to the device.
+        /// </summary>
+        public abstract bool Connect();
+
+        /// <summary> 
+        /// Disconnect the device.
+        /// </summary>
+        public abstract bool Disconnect();
+
+        /// <summary>
+        /// <para>Indicates if connected.</para>
+        /// </summary>
+        public abstract bool IsConnected
+        {
+            get;
+        }
+
+        /// <summary>
+        /// <para>Sends a request.</para>
+        /// </summary>
+        /// <param name="request">The request to be sent.</param>
+        public abstract bool Send(byte[] request);
+
+        /// <summary>
+        /// <para>Recieve the reply.</para>
+        /// </summary>
+        public abstract byte[] RecieveReply();
+    }
+}
--- tests/commandtester/tester.Designer.cs
+++ tests/commandtester/tester.Designer.cs
@@ -28,9 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // tester
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "tester";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NXTLib Command Tester";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
